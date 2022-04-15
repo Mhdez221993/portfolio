80fdfe8e950a897f73dd5ec5aa81ed05ea9d9646
Moises <mhdezcoronado@gmail.com>
2022-04-16 00:18:20 +0200

    Update app structure

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../features/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
