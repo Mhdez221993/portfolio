1e95763f990ec8f7a008fabf01c0f3f0fb4065d3
Moises <mhdezcoronado@gmail.com>
2022-04-16 15:55:20 +0200

    Update layout

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Home.css';
+import footershape from './icons/footershape.svg';
 import Shape1 from './icons/Shape-1.svg';
 import Shape from './icons/Shape.svg';
 import Subtract1 from './icons/Subtract-1.svg';
@@ -18,15 +19,15 @@ function Template7Main() {
 
     <div class="template-7-main screen">
       <div class="overlap-group-container-1">
-        <div class="overlap-group">
-          <div class="cp">Mhdez</div>
-        </div>
-        <div class="overlap-group14">
-          <div class="intro-portfolio-about-contact">
-            <span class="span0">Intro<br /></span><span class="span1">Portfolio<br />About<br />Contact</span>
-          </div>
+        <div class="cp">Mhdez</div>
+        <div class="intro-portfolio-about-contact">
+          <span class="span1">Intro</span>
+          <span class="span1">Portfolio</span>
+          <span class="span1">About</span>
+          <span class="span1">Contact</span>
         </div>
       </div>
+
       <div class="flex-col">
         <div class="overlap-group-container-2">
           <div className="intro-container">
@@ -47,121 +48,130 @@ function Template7Main() {
             </div>
           </div>
         </div>
-        <div class="my-portfolio roboto-bold-scarpa-flow-28px">My Portfolio</div>
-        <div class="overlap-group-container-3">
-          <div class="overlap-group1">
-            <div class="huziio poppins-semi-bold-white-20px">Huzi.io</div>
-          </div>
-          <div class="group-container">
-            <div class="overlap-group3">
-              <div class="soap-soap poppins-semi-bold-white-20px">SoapSoap</div>
-              <div class="group-9">
-                <div class="overlap-group-1">
-                  <div class="htmlcss poppins-medium-mystic-14px">HTML/CSS</div>
-                </div>
-                <div class="overlap-group1-1">
-                  <div class="name poppins-medium-mystic-14px">Ruby on Rails</div>
-                </div>
-                <div class="overlap-group2">
-                  <div class="java-script poppins-medium-mystic-14px">JavaScript</div>
-                </div>
-              </div>
-              <div class="overlap-group-2">
-                <div class="see-the-project">See the project</div>
-              </div>
+        <div className="content-wrapper">
+          <div class="my-portfolio roboto-bold-scarpa-flow-28px">My Portfolio</div>
+          <div class="overlap-group-container-3">
+            <div class="overlap-group1">
+              <div class="huziio poppins-semi-bold-white-20px">Huzi.io</div>
             </div>
-          </div>
-        </div>
-        <div class="overlap-group-container">
-          <div class="overlap-group4">
-            <div class="traviso poppins-semi-bold-white-20px">Traviso</div>
-          </div>
-          <div class="overlap-group5">
-            <div class="carsell poppins-semi-bold-white-20px">Carsell</div>
-          </div>
-        </div>
-        <div class="overlap-group-container">
-          <div class="overlap-group6">
-            <div class="audition poppins-semi-bold-white-20px">Audition</div>
-          </div>
-          <div class="overlap-group7">
-            <div class="weather-app poppins-semi-bold-white-20px">WeatherApp</div>
-          </div>
-        </div>
-        <div class="group-38">
-          <div class="about-me roboto-bold-scarpa-flow-28px">About me</div>
-          <p class="in-three-sentences-d roboto-normal-storm-gray-16px">
-            In three sentences describe who you are as a proffessional. Look through some of my work and experience! If you like what you see
-            and have project <br />you need coded, don’t hesistate to contact me.
-          </p>
-          <div class="overlap-group-3">
-            <div class="get-my-resume roboto-bold-white-16px">Get my resume</div>
-          </div>
-        </div>
-        <div class="group-container-1">
-          <div class="group-37">
-            <div class="languages roboto-bold-scarpa-flow-22px">Languages</div>
-            <p class="java-script-es5es6-ruby-html-cssroboto-normal-storm-gray-16px">
-              JavaScript (ES5/ES6)&nbsp;&nbsp;&nbsp;&nbsp; Ruby<br />HTML<br />CSS
-            </p>
-          </div>
-          <div class="overlap-group9">
-            <div class="frameworks roboto-bold-scarpa-flow-22px">Frameworks</div>
-            <div class="bootstrap-ruby-on-raroboto-normal-storm-gray-16px">
-              Bootstrap<br />Ruby on Rails<br />RSpec<br />Capybara<br />Selenium
-            </div>
-          </div>
-          <div class="overlap-group10">
-            <div class="skills roboto-bold-scarpa-flow-22px">Skills</div>
-            <div class="bootstrap-bulma-coderoboto-normal-storm-gray-16px">
-              Bootstrap<br />Bulma<br />Codekit<br />Codepen<br />Github<br />Gitlab
-            </div>
-          </div>
-        </div>
-        <div class="overlap-group13">
-          <img alt="Damy data" class="footer-shape" src="footer-shape.svg" />
-          <div class="group-13">
-            <div class="interested-in-collaborating roboto-bold-scarpa-flow-28px">Interested in collaborating?</div>
-            <p class="if-you-have-an-appli poppins-normal-storm-gray-16px">
-              If you have an application you are interested in developing, a feature that you need built or a project that need coding, I’d love
-              to help you with it.
-            </p>
-            <div class="overlap-group-container-4">
-              <div class="overlap-group-4">
-                <div class="yeremias poppins-normal-storm-gray-16px">Yeremias</div>
-              </div>
-              <div class="overlap-group2-1border-1px-black-2">
-                <div class="emailvalign-text-middle roboto-normal-manatee-16px">Email*</div>
-              </div>
-            </div>
-            <div class="overlap-group3-1border-1px-black-2">
-              <div class="messagevalign-text-middle roboto-normal-manatee-16px">Message*</div>
-            </div>
-            <div class="group-container-2">
-              <div class="overlap-group1-2">
-                <div class="get-in-touchroboto-bold-white-16px">Get in touch</div>
-              </div>
-              <div class="group-36">
-                <div class="group-15">
-                  <a href="mailto:cg@gmailjob.com" target="_blank" class="align-self-flex-end"
-                  ><div class="cggmailjobcomroboto-normal-periwinkle-16px">cg@gmailjob.com</div></a
-                  >
-                  <div class="flex-row">
-                    <img alt="Damy data" class="github" src="github.png" /><img alt="Damy data" class="twitter" src="twitter.png" /><img alt="Damy data"
-                      class="linked-in"
-                      src="linkedin.png"
-                    />
-                    <div class="group-31"></div>
-                    <div class="group"></div>
-                    <div class="group-32"></div>
-                    <img alt="Damy data" class="flex-row-item" src="facebook.png" /><img alt="Damy data"
-                      class="flex-row-item"
-                      src="instagram.png"
-                    />
+            <div class="group-container">
+              <div class="overlap-group3">
+                <div class="soap-soap poppins-semi-bold-white-20px">SoapSoap</div>
+                <div class="group-9">
+                  <div class="overlap-group-1">
+                    <div class="htmlcss poppins-medium-mystic-14px">HTML/CSS</div>
+                  </div>
+                  <div class="overlap-group1-1">
+                    <div class="name poppins-medium-mystic-14px">Ruby on Rails</div>
+                  </div>
+                  <div class="overlap-group2">
+                    <div class="java-script poppins-medium-mystic-14px">JavaScript</div>
                   </div>
                 </div>
-                <img alt="Damy data" class="vector-4" src="vector-4.svg" />
-                <div class="copyright roboto-normal-periwinkle-16px">© 2020&nbsp;&nbsp;Microverse</div>
+                <div class="overlap-group-2">
+                  <div class="see-the-project">See the project</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="overlap-group-container">
+            <div class="overlap-group4">
+              <div class="traviso poppins-semi-bold-white-20px">Traviso</div>
+            </div>
+            <div class="overlap-group5">
+              <div class="carsell poppins-semi-bold-white-20px">Carsell</div>
+            </div>
+          </div>
+          <div class="overlap-group-container">
+            <div class="overlap-group6">
+              <div class="audition poppins-semi-bold-white-20px">Audition</div>
+            </div>
+            <div class="overlap-group7">
+              <div class="weather-app poppins-semi-bold-white-20px">WeatherApp</div>
+            </div>
+          </div>
+          <div class="group-38">
+            <div class="about-me roboto-bold-scarpa-flow-28px">About me</div>
+            <p class="in-three-sentences-d roboto-normal-storm-gray-16px">
+              In three sentences describe who you are as a proffessional. Look through some of my work and experience! If you like what you see
+              and have project <br />you need coded, don’t hesistate to contact me.
+            </p>
+            <div class="overlap-group-3">
+              <div class="get-my-resume roboto-bold-white-16px">Get my resume</div>
+            </div>
+          </div>
+
+          <div class="group-container-1">
+            <div class="group-37">
+              <div class="languages roboto-bold-scarpa-flow-22px">Languages</div>
+              <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+              <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+              <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+              <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+            </div>
+
+            <div class="group-37">
+              <div class="languages roboto-bold-scarpa-flow-22px">Languages</div>
+              <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+              <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+              <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+              <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+            </div>
+
+            <div class="group-37">
+              <div class="languages roboto-bold-scarpa-flow-22px">Languages</div>
+              <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+              <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+              <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+              <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+            </div>
+          </div>
+
+          <div class="overlap-group13">
+            <img className="footer-shape" src={footershape} />
+            <div class="group-13">
+              <div class="interested-in-collaborating roboto-bold-scarpa-flow-28px">Interested in collaborating?</div>
+              <p class="if-you-have-an-appli poppins-normal-storm-gray-16px">
+                If you have an application you are interested in developing, a feature that you need built or a project that need coding, I’d love
+                to help you with it.
+              </p>
+              <div class="overlap-group-container-4">
+                <div class="overlap-group-4">
+                  <div class="yeremias poppins-normal-storm-gray-16px">Yeremias</div>
+                </div>
+                <div class="overlap-group2-1border-1px-black-2">
+                  <div class="emailvalign-text-middle roboto-normal-manatee-16px">Email*</div>
+                </div>
+              </div>
+              <div class="overlap-group3-1border-1px-black-2">
+                <div class="messagevalign-text-middle roboto-normal-manatee-16px">Message*</div>
+              </div>
+              <div class="group-container-2">
+                <div class="overlap-group1-2">
+                  <div class="get-in-touchroboto-bold-white-16px">Get in touch</div>
+                </div>
+                <div class="group-36">
+                  <div class="group-15">
+                    <a href="mailto:cg@gmailjob.com" target="_blank" class="align-self-flex-end"
+                    ><div class="cggmailjobcomroboto-normal-periwinkle-16px">cg@gmailjob.com</div></a
+                    >
+                    <div class="flex-row">
+                      <img alt="Damy data" class="github" src="github.png" /><img alt="Damy data" class="twitter" src="twitter.png" /><img alt="Damy data"
+                        class="linked-in"
+                        src="linkedin.png"
+                      />
+                      <div class="group-31"></div>
+                      <div class="group"></div>
+                      <div class="group-32"></div>
+                      <img alt="Damy data" class="flex-row-item" src="facebook.png" /><img alt="Damy data"
+                        class="flex-row-item"
+                        src="instagram.png"
+                      />
+                    </div>
+                  </div>
+                  <img alt="Damy data" class="vector-4" src="vector-4.svg" />
+                  <div class="copyright roboto-normal-periwinkle-16px">© 2020&nbsp;&nbsp;Microverse</div>
+                </div>
               </div>
             </div>
           </div>
