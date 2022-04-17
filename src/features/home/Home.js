a3b857571bddc6c111ce019e5f28ceb536685d41
Moises <mhdezcoronado@gmail.com>
2022-04-17 19:19:29 +0200

    Apply contact form

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -172,23 +172,22 @@ function Template7Main() {
               </p>
             </div>
 
-            <form class="overlap-group-container-4">
+            <form class="overlap-group-container-4" action="https://formspree.io/f/xknyqkgk" method="POST">
               <div className="input-wrapper">
                 <div class="inputs">
-                  <input class="overlap-group-4 poppins-normal-storm-gray-16px" placeholder="Name*" />
-                  <input class="overlap-group-4 poppins-normal-storm-gray-16px" placeholder="Email*" />
+                  <input class="overlap-group-4 poppins-normal-storm-gray-16px" placeholder="Name*" name="Name" id="name" type="text" />
+                  <input class="overlap-group-4 poppins-normal-storm-gray-16px" placeholder="Email*" name="Email" id="email" type="email" />
                 </div>
 
                 <div class="overlap-group3-1border-1px-black-2">
-                  <textarea class="message roboto-normal-manatee-16px">Message*</textarea>
+                  <textarea class="message roboto-normal-manatee-16px" name="message" id="message" type="text" placeholder="Message*"></textarea>
                 </div>
               </div>
 
               <div class="overlap-group1-2">
-                <button type="submit" class="get-in-touch roboto-bold-white-16px">Get in touch</button>
+                <button type="submit" value="Submit" class="get-in-touch roboto-bold-white-16px">Get in touch</button>
                 <a href="mailto:cg@gmailjob.com" target="_blank" class="align-self-flex-end"
-                ><div class="cggmailjobcom roboto-normal-periwinkle-16px">mhdezcoronado@gmail.com</div></a
-                >
+                ><div class="cggmailjobcom roboto-normal-periwinkle-16px">mhdezcoronado@gmail.com</div></a>
               </div>
             </form>
 
