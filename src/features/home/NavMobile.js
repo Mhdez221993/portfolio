df0dca998a41ef935996381e00fea76f21b61485
Moises <mhdezcoronado@gmail.com>
2022-04-18 15:21:54 +0200

    Create Nav component

---
+++ src/features/home/NavMobile.js
@@ -0,0 +1,12 @@
+function NavMobile() {
+  return (
+    <div class="mobile-heder">
+      <div class="cp "></div>
+      <a className="span1" href="#portfolio">Portfolio</a>
+      <a className="span1" href="#about">About</a>
+      <a className="span1" href="#footer">Contact</a>
+    </div>
+  );
+}
+
+export default NavMobile;
