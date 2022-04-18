477643b061487bfe969965144b0fc3bf488f7bd9
Moises <mhdezcoronado@gmail.com>
2022-04-18 15:21:42 +0200

    Create NavMobile component

---
+++ src/features/home/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div class="overlap-group-container-1" id="intro">
+      <div class="cp"></div>
+      <div class="intro-portfolio-about-contact">
+        <a className="span1" href="#intro">Intro</a>
+        <a className="span1" href="#portfolio">Portfolio</a>
+        <a className="span1" href="#about">About</a>
+        <a className="span1" href="#footer">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
