1cf6e1c34dc3c77b743b79bfc2d1cf4a23477105
Moises <mhdezcoronado@gmail.com>
2022-04-18 15:22:05 +0200

    Create Skills component

---
+++ src/features/home/Skills.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <div class="group-container-1">
+      <div class="group-37">
+        <div class="languages roboto-bold-scarpa-flow-22px">Languages</div>
+        <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+        <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+        <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+        <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+      </div>
+
+      <div class="group-37">
+        <div class="languages roboto-bold-scarpa-flow-22px">Framewors</div>
+        <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+        <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+        <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+        <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+      </div>
+
+      <div class="group-37">
+        <div class="languages roboto-bold-scarpa-flow-22px">Skills</div>
+        <div class="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
+        <div class="languages roboto-normal-storm-gray-16px">Ruby</div>
+        <div class="languages roboto-normal-storm-gray-16px">HTML</div>
+        <div class="languages roboto-normal-storm-gray-16px">CSS</div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
