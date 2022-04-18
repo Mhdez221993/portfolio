d7f287bed630f118d509a0a4f03bdd44b382bd54
Moises <mhdezcoronado@gmail.com>
2022-04-18 15:20:22 +0200

    Create AboutMe component

---
+++ src/features/home/AboutMe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div class="group-38" id="about">
+      <div class="about-me roboto-bold-scarpa-flow-28px">About me</div>
+      <p class="in-three-sentences-d roboto-normal-storm-gray-16px">
+        In three sentences describe who you are as a proffessional. Look through some of my work and experience! If you like what you see
+        and have project <br />you need coded, don’t hesistate to contact me.
+      </p>
+      <div class="overlap-group-3">
+        <button type="button" class="get-my-resume roboto-bold-white-16px">Get my resume</button>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
