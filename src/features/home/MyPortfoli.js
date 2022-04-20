8fcb8951f2eecaa6095fc7bd23b742eb36469193
Moises <mhdezcoronado@gmail.com>
2022-04-21 00:28:31 +0200

    Update Profolio component

--- src/features/home/MyPortfoli.js
+++ src/features/home/MyPortfoli.js
@@ -1,38 +1,58 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes, { string } from 'prop-types';
+import React, { useState } from 'react';
+import Details from './Details';
 
-function MyPortfolio({ displayDitail }) {
+function MyPortfolio({ project }) {
+  const [ditail, setDisplayDitails] = useState(false);
+  const displayDitail = () => {
+    setDisplayDitails(true);
+  };
+
+  const {
+    title,
+    techStack,
+    backgroundImage,
+  } = project;
+
   return (
-    <div className="overlap-group-container-3">
-      <div className="overlap-group3">
-        <div className="soap-soap poppins-semi-bold-white-20px">SoapSoap</div>
-        <div className="group-9">
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby on Rails</div>
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby</div>
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby on Rails</div>
-        </div>
-        <div className="overlap-group-2">
-          <button onClick={displayDitail} type="button" className="see-the-project">See the project</button>
-        </div>
-      </div>
+    <>
+      {ditail && <Details setDisplayDitails={setDisplayDitails} />}
 
-      <div className="overlap-group3">
-        <div className="soap-soap poppins-semi-bold-white-20px">SoapSoap</div>
+      <div
+        key={title}
+        className="overlap-group3"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="soap-soap poppins-semi-bold-white-20px">{title}</div>
         <div className="group-9">
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby on Rails</div>
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby</div>
-          <div className="overlap-group1-1 poppins-medium-mystic-14px">Ruby on Rails</div>
+          {
+            techStack.map((stack) => (
+              <div
+                key={stack}
+                className="overlap-group1-1 poppins-medium-mystic-14px"
+              >
+                {stack}
+              </div>
+            ))
+          }
         </div>
         <div className="overlap-group-2">
           <button onClick={displayDitail} type="button" className="see-the-project">See the project</button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
 MyPortfolio.propTypes = {
-  displayDitail: PropTypes.func.isRequired,
+  // displayDitail: PropTypes.func.isRequired,
+  project: PropTypes.shape({
+    title: string.isRequired,
+    backgroundImage: string.isRequired,
+    techStack: PropTypes.shape([
+      string.isRequired,
+    ]),
+  }).isRequired,
 };
 
 export default MyPortfolio;
