eddc09ba23dc77f5eb9d251a9c9ac48a940c3595
Moises <mhdezcoronado@gmail.com>
2022-04-20 22:17:29 +0200

    Create methos to swap images

--- src/features/home/Details.js
+++ src/features/home/Details.js
@@ -1,40 +1,82 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import './Details.css';
 import Fill4 from './icons/Fill4.svg';
 import group18 from './icons/Group18.svg';
 import Group41 from './icons/Group41.svg';
+import image3 from './icons/image3.png';
 import image4 from './icons/image4.png';
+import image5 from './icons/image5.png';
 
 function Details({ setDisplayDitails }) {
+  const image = 'https://github.com/Mhdez221993/portfolio/blob/main/src/features/home/icons/project.png?raw=true';
+
+  const [state, setState] = useState(image);
+
+  const [state1, setState1] = useState(image3);
+  const [state2, setState2] = useState(image4);
+  const [state3, setState3] = useState(image5);
+
   const nonDisplay = () => {
     setDisplayDitails(false);
+  };
+
+  const swapImages = (newImg, cb) => {
+    setState(newImg);
+    cb(state);
   };
 
   return (
     <div className="overlap-group8">
       <div className="rectangle-16">
         <button onClick={nonDisplay} onKeyDown={nonDisplay} type="button" className="closeModal">
-          <img src={group18} alt="group18" />
+          <img src={group18} alt="group18" className="close" />
         </button>
-        <div className="project-image">
+        <div
+          className="project-image"
+          style={{
+            backgroundImage: `url(${state})`,
+          }}
+        >
           <div className="thomnail-warpper">
 
             <div className="thomenail1">
-              <img className="thom-image" src={image4} alt="thom-nail" />
-              <img className="thom-image" src={image4} alt="thom-nail" />
-              <img className="thom-image" src={image4} alt="thom-nail" />
+
+              <button type="button" onClick={() => swapImages(state1, setState1)}>
+                <img
+                  className="thom-image"
+                  src={state1}
+                  alt="thom-nail"
+                />
+              </button>
+
+              <button type="button" onClick={() => swapImages(state2, setState2)}>
+                <img
+                  className="thom-image"
+                  src={state2}
+                  alt="thom-nail"
+                />
+              </button>
+
+              <button type="button" onClick={() => swapImages(state3, setState3)}>
+                <img
+                  className="thom-image"
+                  src={state3}
+                  alt="thom-nail"
+                />
+              </button>
+
             </div>
 
             <div className="see-live-source">
               <button type="button" className="see-source">
                 See source
-                <img src={Fill4} alt="Fill4" />
+                <img src={Fill4} alt="Fill4" className="source-img" />
               </button>
 
               <button type="button" className="see-source">
                 See Live
-                <img src={Group41} alt="Group41" />
+                <img src={Group41} alt="Group41" className="live-img" />
               </button>
             </div>
 
