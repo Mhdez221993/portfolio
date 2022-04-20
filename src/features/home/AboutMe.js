cdc87bba7cc89bef725a38922874deeddd24cbf4
Moises <mhdezcoronado@gmail.com>
2022-04-20 15:08:47 +0200

    Update AboutMe component

--- src/features/home/AboutMe.js
+++ src/features/home/AboutMe.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import DownloadResume from './DownloadResume';
 
 function AboutMe() {
   return (
     <div className="group-38" id="about">
       <div className="about-me roboto-bold-scarpa-flow-28px">About me</div>
       <p className="in-three-sentences-d roboto-normal-storm-gray-16px">
-        In three sentences describe who you are as a
-        proffessional. Look through some of my work and experience! If you like what you see
-        and have project
-        {' '}
+        Hi there! my name is Moises I&apos;m a full-stack software engineer.
+        Specializing in using Ruby on Rails, Javascript,
+        and React to build software solutions and web applications.
+        I&apos;m a self-motivated learner fluent in Spanish
+        and English and currently learning Italian.
+        I&apos;m a pair programming, remote work enthusiast, and a good team player.
         <br />
-        you need coded, don’t hesistate to contact me.
+        You can find me solving algorithms, contributing on Github, or just cooking some pasta.
       </p>
-      <div className="overlap-group-3">
-        <button type="button" className="get-my-resume roboto-bold-white-16px">Get my resume</button>
-      </div>
+      <DownloadResume />
     </div>
   );
 }
