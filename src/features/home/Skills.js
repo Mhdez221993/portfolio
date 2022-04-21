06e0917b84d91e7150d8f1e7e772f476c6baa7dd
Moises <mhdezcoronado@gmail.com>
2022-04-21 18:25:04 +0200

    Update Skill component

--- src/features/home/Skills.js
+++ src/features/home/Skills.js
@@ -13,18 +13,20 @@ function Skills() {
 
       <div className="group-37">
         <div className="languages roboto-bold-scarpa-flow-22px">Framewors</div>
-        <div className="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
-        <div className="languages roboto-normal-storm-gray-16px">Ruby</div>
-        <div className="languages roboto-normal-storm-gray-16px">HTML</div>
-        <div className="languages roboto-normal-storm-gray-16px">CSS</div>
+        <div className="languages roboto-normal-storm-gray-16px">Boostrap</div>
+        <div className="languages roboto-normal-storm-gray-16px">Ruby on Rails</div>
+        <div className="languages roboto-normal-storm-gray-16px">RSpec</div>
+        <div className="languages roboto-normal-storm-gray-16px">Jest</div>
+        <div className="languages roboto-normal-storm-gray-16px">Capybara</div>
       </div>
 
       <div className="group-37">
         <div className="languages roboto-bold-scarpa-flow-22px">Skills</div>
-        <div className="languages roboto-normal-storm-gray-16px">JavaScript (ES5/ES6)</div>
-        <div className="languages roboto-normal-storm-gray-16px">Ruby</div>
-        <div className="languages roboto-normal-storm-gray-16px">HTML</div>
-        <div className="languages roboto-normal-storm-gray-16px">CSS</div>
+        <div className="languages roboto-normal-storm-gray-16px">GitHub</div>
+        <div className="languages roboto-normal-storm-gray-16px">Boostrap</div>
+        <div className="languages roboto-normal-storm-gray-16px">Git</div>
+        <div className="languages roboto-normal-storm-gray-16px">CI/CD</div>
+        <div className="languages roboto-normal-storm-gray-16px">GitFlow</div>
       </div>
     </div>
   );
