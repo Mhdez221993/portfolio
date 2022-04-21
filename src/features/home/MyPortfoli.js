3f5f4c7f2661d7cb40ca115199b724b0ea3333ee
Moises <mhdezcoronado@gmail.com>
2022-04-21 17:50:03 +0200

    Update MyPortfolio with props

--- src/features/home/MyPortfoli.js
+++ src/features/home/MyPortfoli.js
@@ -1,13 +1,7 @@
 import PropTypes, { string } from 'prop-types';
-import React, { useState } from 'react';
-import Details from './Details';
+import React from 'react';
 
-function MyPortfolio({ project }) {
-  const [ditail, setDisplayDitails] = useState(false);
-  const displayDitail = () => {
-    setDisplayDitails(true);
-  };
-
+function MyPortfolio({ project, displayDitail }) {
   const {
     title,
     techStack,
@@ -15,37 +9,33 @@ function MyPortfolio({ project }) {
   } = project;
 
   return (
-    <>
-      {ditail && <Details setDisplayDitails={setDisplayDitails} />}
-
-      <div
-        key={title}
-        className="overlap-group3"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <div className="soap-soap poppins-semi-bold-white-20px">{title}</div>
-        <div className="group-9">
-          {
-            techStack.map((stack) => (
-              <div
-                key={stack}
-                className="overlap-group1-1 poppins-medium-mystic-14px"
-              >
-                {stack}
-              </div>
-            ))
-          }
-        </div>
-        <div className="overlap-group-2">
-          <button onClick={displayDitail} type="button" className="see-the-project">See the project</button>
-        </div>
+    <div
+      key={title}
+      className="overlap-group3"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="soap-soap poppins-semi-bold-white-20px">{title}</div>
+      <div className="group-9">
+        {
+          techStack.map((stack) => (
+            <div
+              key={stack}
+              className="overlap-group1-1 poppins-medium-mystic-14px"
+            >
+              {stack}
+            </div>
+          ))
+        }
       </div>
-    </>
+      <div className="overlap-group-2">
+        <button onClick={displayDitail} type="button" className="see-the-project">See the project</button>
+      </div>
+    </div>
   );
 }
 
 MyPortfolio.propTypes = {
-  // displayDitail: PropTypes.func.isRequired,
+  displayDitail: PropTypes.func.isRequired,
   project: PropTypes.shape({
     title: string.isRequired,
     backgroundImage: string.isRequired,
