596cc8185197d812eb80820fe4d3df3b5d978c75
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-20 18:47:43 +0000

    Remove unused social media links from footer and header components

--- src/features/footer/MediaFooter.js
+++ src/features/footer/MediaFooter.js
@@ -1,23 +1,13 @@
 import React from "react";
-import facebook from "./icons/facebook.svg";
 import github from "./icons/github.svg";
 import group31 from "./icons/group31.svg";
 import group32 from "./icons/group32.svg";
-import twitter from "./icons/twitter.svg";
 
 function MediaFooter() {
   return (
     <div className="social-media-footer">
       <a href="https://github.com/Mhdez221993" target="_blank" rel="noreferrer">
         <img src={github} className="union" alt="union" />
-      </a>
-
-      <a
-        href="https://twitter.com/MoisesH42060050"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={twitter} className="union" alt="union" />
       </a>
 
       <a
@@ -34,14 +24,6 @@ function MediaFooter() {
         rel="noreferrer"
       >
         <img src={group31} className="union" alt="union" />
-      </a>
-
-      <a
-        href="https://www.facebook.com/moises.hernandezcoronado"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={facebook} className="union" alt="union" />
       </a>
     </div>
   );
--- src/features/header/MediaHeader.js
+++ src/features/header/MediaHeader.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Shape from "./icons/Shape.svg";
-import Shape1 from "./icons/Shape-1.svg";
-import Subtract1 from "./icons/Subtract-1.svg";
 import Subtract2 from "./icons/Subtract-2.svg";
 import Union from "./icons/Union.svg";
 import Union1 from "./icons/Union-1.svg";
@@ -29,22 +27,6 @@ function MediaHeader() {
         rel="noreferrer"
       >
         <img src={Shape} className="union" alt="union" />
-      </a>
-
-      <a
-        href="https://twitter.com/MoisesH42060050"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={Shape1} className="union" alt="union" />
-      </a>
-
-      <a
-        href="https://www.facebook.com/moises.hernandezcoronado"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={Subtract1} className="union" alt="union" />
       </a>
 
       <a
