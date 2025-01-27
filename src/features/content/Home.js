883d47f1bb45e4989aa5b3a1cd4b3c4398bc8e96
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-27 13:32:59 +0000

    Enhance Home component to load and display components data alongside projects, integrating componentsAsync for improved state management

--- src/features/content/Home.js
+++ src/features/content/Home.js
@@ -1,7 +1,7 @@
 import "./HomeDesktop.css";
 
-import React, { useState } from "react";
-import { projectsAsync, setProject } from "./homeSlice";
+import React, { useState, useEffect } from "react";
+import { componentsAsync, projectsAsync, setProject } from "./homeSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 import AboutMe from "./AboutMe";
@@ -13,18 +13,23 @@ function Content() {
   const dispatch = useDispatch();
 
   const [ditail, setDisplayDitails] = useState(false);
-  const { projects } = useSelector(({ home }) => home);
+  const { projects, components } = useSelector(({ home }) => home);
+  console.log(components);
 
   const displayDitail = (project) => {
     dispatch(setProject(project));
     setDisplayDitails(true);
   };
 
-  useState(() => {
+  useEffect(() => {
     if (!projects.length) {
       dispatch(projectsAsync());
     }
-  });
+
+    if (!components.length) {
+      dispatch(componentsAsync());
+    }
+  }, [dispatch, projects.length, components.length]);
 
   return (
     <div className="content-wrapper">
@@ -34,7 +39,28 @@ function Content() {
         className="my-portfolio roboto-bold-scarpa-flow-28px padding"
         id="portfolio"
       >
-        My Portfolio
+        Components
+      </div>
+
+      <div className="components">
+        {components.map(({ title, backgroundImage }) => (
+          <div
+            key={title}
+            className="component"
+            style={{
+              backgroundImage: `url(${backgroundImage})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+        ))}
+      </div>
+
+      <div
+        className="my-portfolio roboto-bold-scarpa-flow-28px padding"
+        id="portfolio"
+      >
+        Apps
       </div>
 
       <div className="projects">
