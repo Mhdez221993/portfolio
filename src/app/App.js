7b01c14d3d9c6aee9b800f04b213f3a01559b5d9
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-27 13:30:41 +0000

    Remove Router component from App.js for simplified structure

--- src/app/App.js
+++ src/app/App.js
@@ -4,17 +4,14 @@ import Content from "../features/content/Home";
 import Footer from "../features/footer/Footer";
 import Header from "../features/header/Header";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="app">
-        <Header />
-        <Content />
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   );
 }
 
