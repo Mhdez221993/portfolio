a6f7d72f6aa59fe0c6b59956e620c04ab55c04de
Moises Hernandez <mhdezcoronado@gmail.com>
2025-01-28 16:38:46 +0000

    Add Blog card component to components API

--- src/features/content/componentsAPI.js
+++ src/features/content/componentsAPI.js
@@ -5,6 +5,12 @@ const components = [
       "https://github.com/Mhdez221993/testimonial-card/blob/main/img/card.png?raw=true",
     link: "https://mhdez221993.github.io/testimonial-card/",
   },
+  {
+    title: "Blog card",
+    backgroundImage:
+      "https://github.com/Mhdez221993/blog-card/blob/main/img/card.png?raw=true",
+    link: "https://github.com/Mhdez221993/blog-card",
+  },
 ];
 
 const fetchComponents = () =>
